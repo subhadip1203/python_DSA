f46083a31dfb64153cc9a5a0b87626dda9c57af5
subhadip1203 <subhadip1203@gmail.com>
2021-05-09 23:27:50 +0530

    stack & queue :basics

---
+++ queue/basic.py
@@ -0,0 +1,14 @@
+queue = ["a", "b", "c"]
+queue.append("d")
+queue.append("e")
+print(queue)
+  
+# Removes the first item
+print(queue.pop(0))
+  
+print(queue)
+  
+# Removes the first item
+print(queue.pop(0))
+  
+print(queue)
