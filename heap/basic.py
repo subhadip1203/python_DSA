ef44fd24172d9db0da5a4820ba7fd04612e802ec
subhadip1203 <subhadip1203@gmail.com>
2021-10-21 23:55:23 +0530

    heap with tuple

--- heap/basic.py
+++ heap/basic.py
@@ -11,3 +11,22 @@ print (list(arr))
 
 heapq.heappop(arr)
 print (list(arr))
+
+
+# heap for tuples , only first elemnt will be considered
+# tuple index zero [0] , will be consider for assending order
+
+arr2 = []
+heapq.heapify(arr2)
+
+heapq.heappush(arr2,(10,25))
+print (list(arr2))
+
+heapq.heappush(arr2,(1,25))
+print (list(arr2))
+
+heapq.heappush(arr2,(10,20))
+print (list(arr2))
+
+heapq.heappush(arr2,(9,20))
+print (list(arr2))
