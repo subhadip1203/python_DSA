34a60430efd0e82b3388045dfe7ada92a5e23ee2
subhadip1203 <subhadip1203@gmail.com>
2022-07-14 23:11:22 +0530

    py collection types

---
+++ array/dataTypes.py
@@ -0,0 +1,18 @@
+
+#List 
+a = [1,2,3,4,5]
+
+# dict : key value pair
+b = {
+  "key" : "value"
+}
+
+# tuple: unchangeable collection.
+# can not add or delete after creating it
+# to change it , first convert into list 
+# then again convert into tuple
+c = ("banana", "apple", "cherry")
+
+
+#set : unordered and unindexed.
+myset = { "banana", "apple","cherry"}
