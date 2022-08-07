c1941fd3cbab68c3dfa2636f239950e6ab6344eb
subhadipsjsc <subhadipsjsc@gmail.com>
2022-08-07 15:34:05 -0700

    class-obj pointers

--- class-object/myClass.py
+++ class-object/myClass.py
@@ -5,5 +5,13 @@ class Node:
 
 myObj = Node()
 myObj.data = 10
+
+
+secondObj = Node()
+secondObj.data=10
+myObj.next = secondObj
+
 print(myObj) 		# it will give only memory address 
 print(myObj.data)	# it will be a value
+print(myObj.next)	# it will give only memory address 
+print(myObj.next.data)	# it will be a value
